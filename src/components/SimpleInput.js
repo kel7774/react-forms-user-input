d598265f702cf7a09bcfb5a396b4cc992a11239a
Kelli Landry <landry.kel@gmail.com>
2021-06-10 18:10:32 -0500

    trash if statements, store validity in variable, make it lean

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,21 +1,42 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
+
 const SimpleInput = (props) => {
-  const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState('')
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+  const enteredNameIsValid = enteredName.trim() !== ''
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true)
+  }
+
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value)
   }
+
   const formSubmissionHandler = event => {
     event.preventDefault()
-    const enteredValue = nameInputRef.current.value
-    console.log(enteredValue)
+    setEnteredNameTouched(true)
+    if (!enteredNameIsValid) {
+      return null
+    }
     setEnteredName('')
+    setEnteredNameTouched(false)
   }
+
+  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} value={enteredName} type='text' id='name' onChange={nameInputChangeHandler} />
+        <input
+          value={enteredName}
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+        />
+        {nameInputIsInvalid ? <p className='error-text'>Name must not be empty.</p> : null}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
