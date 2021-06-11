008f673c5af175972e7ca94bc7740472268518bd
Kelli Landry <landry.kel@gmail.com>
2021-06-10 20:54:14 -0500

    add logic w/ hook to basic form component

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,19 +1,65 @@
+import useInput from '../hooks/use-input'
+
 const BasicForm = (props) => {
+  const {
+    value: enteredFirstName,
+    isValid: validFirstNameInput,
+    hasError: inputFirstNameError,
+    valueChangeHandler: inputFirstNameChange,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstNameInput
+  } = useInput((value) => value.trim() !== '')
+
+  const {
+    value: enteredLastName,
+    isValid: validLastNameInput,
+    hasError: inputLastNameError,
+    valueChangeHandler: inputLastNameChange,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastNameInput
+  } = useInput((value) => value.trim() !== '')
+
+  const {
+    value: enteredEmail,
+    isValid: validEmailInput,
+    hasError: inputEmailError,
+    valueChangeHandler: inputEmailChange,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput
+  } = useInput((value) => value.includes('@'))
+
+  let formIsValid = false
+  if (validFirstNameInput && validLastNameInput && validEmailInput) {
+    formIsValid = true
+  }
+  const submitForm = event => {
+    event.preventDefault()
+    if (!formIsValid) {
+      return null
+    }
+    resetFirstNameInput()
+    resetLastNameInput()
+    resetEmailInput()
+  }
+
   return (
-    <form>
+    <form onSubmit={submitForm}>
       <div className='control-group'>
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' value={enteredFirstName} onChange={inputFirstNameChange} onBlur={firstNameBlurHandler} />
+          {inputFirstNameError ? <p>Please enter a first name</p> : null}
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' value={enteredLastName} onChange={inputLastNameChange} onBlur={lastNameBlurHandler} />
+          {inputLastNameError ? <p>Please enter a first name</p> : null}
         </div>
       </div>
       <div className='form-control'>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' value={enteredEmail} onChange={inputEmailChange} onBlur={emailBlurHandler} />
+        {inputEmailError ? <p>Please enter a first name</p> : null}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -27,7 +27,7 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault()
 
-    if (!enteredNameIsValid) {
+    if (!formIsValid) {
       return
     }
 
